f9c4b5256d486f3d1076a89e960ed7d773640649
AugustHan <alicehanjingjing@gmail.com>
2021-09-09 22:14:40 -0700

    20210901

--- js+html+css/first_login.js
+++ js+html+css/first_login.js
@@ -1,1 +1,7 @@
 console.log("This is a javascript!!!");
+function firstJsFunction() {
+	console.log("This is firstJsFunction!");
+    let intros = document.getElementsByClassName('intro');
+    console.log(intros);
+	alert("firstJsFunction!");
+}
